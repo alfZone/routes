6be7be6001893315379061d8f7800dd90cb8a228
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2023-06-01 19:14:37 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+define("_SERVER", 'localhost');
+define("_BDUSER", 'USERNAME');
+define("_BDPASS", 'PASSWORD');
+define("_BD", 'DBNAME');
+
+define("_CAMINHO_TEMPLATE", '../templates/base/');
+define("_CAMINHO_ADM", '../admin/');
+
+?>
