725a4a535c669340fd77259259e797c71423e2c3
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2024-11-19 12:13:27 +0000

    Update Connection.php

--- src/Connection.php
+++ src/Connection.php
@@ -1,6 +1,23 @@
 <?php
-namespace HeroProject;
-class Connection
-{
+namespace src;
+use PDO;
+
+class Connection{
   // put the database stuffs here in that scope
+  private $host = _SERVER;
+  private $db_name = _BDUSER;
+  private $username = _BD;
+  private $password = _BDPASS;
+  public $conn;
+
+  public function getConnection() {
+      $this->conn = null;
+      try {
+          $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+          $this->conn->exec("set names utf8");
+      } catch (PDOException $exception) {
+          echo "Erro na conexão: " . $exception->getMessage();
+      }
+      return $this->conn;
+  }
 }
