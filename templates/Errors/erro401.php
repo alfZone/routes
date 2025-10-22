ae485b1b1ce1e2f3e5da38fa309eaf1b196cdda0
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2025-10-22 09:10:23 +0100

    Add files via upload

---
+++ templates/Errors/erro401.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page no found</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class=bg-black>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+           <img src="/templates/Errors/images/401.png" class="img-fluid" alt="Page not Found" width="100%">
+          <a href="<?=_URL?>/administrator"><button>Voltar</button></a> 
+        </div>
+      </div>
+    </div>
+   
+    
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
