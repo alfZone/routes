9d05064fa7d7be5653fe96331948186124a69061
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2025-10-31 15:41:20 +0000

    Add JSON_UNESCAPED_UNICODE to JSON responses

--- app/ControllerCars.php
+++ app/ControllerCars.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * @autores alf
+ * @copyright 2025
+ * @ver 2.0
+ */
+
+
 namespace app;
 use src\Connection;
 use PDO;
@@ -18,16 +26,16 @@ class ControllerCarros {
     // Obter todos os carros
     public function getAll() {
         $carros = $this->database->getData("SELECT * FROM alfCarros");
-        echo json_encode($carros);
+        echo json_encode($carros, JSON_UNESCAPED_UNICODE);
     }
 
     // Obter carro por ID
     public function getById($id) {
         $p['id']=$id;
         $carro = $this->database->getData("SELECT * FROM alfCarros WHERE id = :id", $p);
-        print_r($carro);
+        //print_r($carro);
         if ($carro) {
-            echo json_encode($carro);
+            echo json_encode($carro, JSON_UNESCAPED_UNICODE);
         } else {
             echo json_encode(['msg' => 'Carro não encontrado', 'status' => '404']);
         }
@@ -39,7 +47,7 @@ class ControllerCarros {
         $p['detalhes']=$_POST['Detalhes'];
         $p['foto']=$_POST['Foto'];
         $resp = $this->database->setData("INSERT INTO alfCarros (marca, detalhes, foto) VALUES (:marca, :detalhes, :foto)", $p);
-        echo json_encode($resp);
+        echo json_encode($resp, JSON_UNESCAPED_UNICODE);
     }
 
     // Atualizar um carro
