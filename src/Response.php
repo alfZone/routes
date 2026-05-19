56f31366ec28b7bc80b115a681e661befa5d31df
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2026-05-19 14:59:37 +0100

    Update Response.php

--- src/Response.php
+++ src/Response.php
@@ -1,14 +1,42 @@
 <?php
 
+//version 1.0
+// 2026/05/31
+
 namespace src;
 
 class Response
 {
 	
 
-	public function __construct()
-	{
+	public function __construct(){
 
+	}
+
+	public function ok($msg,$data=""){
+		$resp = [
+			'success' => true,  // boolean, não string
+			'message' => $msg,
+			'code' => 200,
+			'timestamp' => date('Y-m-d H:i:s'),
+			'data' => $data,              // para respostas de sucesso
+		];
+		echo json_encode($resp, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+	}
+
+	public function data($data=""){
+		echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+	}
+
+	public function error($msg, $code = 500, $error=""){
+		$resp = [
+			'success' => false,
+			'message' => $msg,
+			'code' => $code,
+			'timestamp' => date('Y-m-d H:i:s'),
+			'error' => $error
+		];
+		echo json_encode($resp, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 	}
 
 	public function header(){
