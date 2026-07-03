49c0a1e63e2695ce51221a817af93af6a04ebc1f
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2026-07-03 14:42:21 +0100

    Update Route.php

--- src/Route.php
+++ src/Route.php
@@ -10,6 +10,66 @@
 
 namespace src;
 
+use src\Router;
+
+class Route
+{
+	protected static $router;
+
+	private function __construct(){
+    
+  }
+
+	protected static function getRouter()	{
+		if(empty(self::$router)) {
+			self::$router = new Router;
+		}
+		return self::$router;
+	}
+
+	public static function post($pattern, $callback){
+		return self::getRouter()->post($pattern, $callback);
+	}
+
+	public static function get($pattern, $callback){
+    //echo "aqui";
+		return self::getRouter()->get($pattern, $callback);
+	}
+
+	public static function put($pattern, $callback){
+		return self::getRouter()->put($pattern, $callback);
+	}
+
+	public static function delete($pattern, $callback){
+		return self::getRouter()->delete($pattern, $callback);
+	}
+
+	public static function resolve($pattern){
+		return self::getRouter()->resolve($pattern);
+	}
+
+	public static function translate($pattern, $params){
+		return self::getRouter()->translate($pattern, $params);
+	}
+
+}
+
+
+
+
+?>
+<?php
+/**
+ * The idea for this object is to provide a simple way routing.
+ * @author António Lira Fernandes
+ * @version 1.1
+ * @updated 04-08-2025 21:50:00
+ * https://github.com/alfZone/routes/
+ * 
+ */
+
+namespace src;
+
 use src\Request;
 use src\Dispacher;
 use src\RouteCollection;
