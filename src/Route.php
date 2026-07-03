f4c8fbc6f28033a9af73105c8a669c591b8c2dd2
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2026-07-03 13:52:30 +0100

    Refactor Route class to Router and enhance methods

--- src/Route.php
+++ src/Route.php
@@ -1,58 +1,103 @@
 <?php
+/**
+ * The idea for this object is to provide a simple way routing.
+ * @author António Lira Fernandes
+ * @version 1.1
+ * @updated 04-08-2025 21:50:00
+ * https://github.com/alfZone/routes/
+ * 
+ */
 
 namespace src;
 
-use src\Router;
+use src\Request;
+use src\Dispacher;
+use src\RouteCollection;
 
-class Route
+class Router 
 {
-	protected static $router;
+	
+	protected $route_collection;
+	public $dispacher;
 
-	private function __construct(){
-    
-  }
+	public function __construct(){
+		$this->route_collection = new RouteCollection;
+		$this->dispacher = new Dispacher;
+	}
 
-	protected static function getRouter()	{
-		if(empty(self::$router)) {
-			self::$router = new Router;
+	public function get($pattern, $callback)	{	
+		$this->route_collection->add('get', $pattern, $callback);
+		return $this;
+	}
+
+	public function post($pattern, $callback)	{	
+		$this->route_collection->add('post', $pattern, $callback);
+		return $this;	
+	}
+
+	public function put($pattern, $callback)	{	
+		$this->route_collection->add('put', $pattern, $callback);
+		return $this;	
+	}
+
+	public function delete($pattern, $callback)	{
+		$this->route_collection->add('delete', $pattern, $callback);
+		return $this;	
+	}
+
+	public function find($request_type, $pattern)	{
+		return $this->route_collection->where($request_type, $pattern);
+	}
+	
+	protected function dispach($route, $params, $namespace = "app\\"){
+		return $this->dispacher->dispach($route->callback, $params, $namespace);
+	}
+	
+	public function notFound(){
+		return header("HTTP/1.0 404 Not Found", true, 404);
+	}
+
+
+	public function resolve($request){	
+		$route = $this->find($request->method(), $request->uri());
+		if($route){
+			$params = $route->callback['values'] ? $this->getValues($request->uri(), $route->callback['values']) : [];
+			return $this->dispach($route, $params);
 		}
-		return self::$router;
+		return $this->notFound();
+
 	}
 
-	public static function post($pattern, $callback){
-		return self::getRouter()->post($pattern, $callback);
+	protected function getValues($pattern, $positions){
+		$result = [];
+		$pattern = array_filter(explode('/', $pattern));
+		foreach($pattern as $key => $value){
+			if(in_array($key, $positions)) {
+				$result[array_search($key, $positions)] = $value;
+			}
+		}
+		return $result;	
 	}
 
-	public static function get($pattern, $callback){
-    //echo "aqui";
-		return self::getRouter()->get($pattern, $callback);
+	public function translate($name, $params)	{
+		$pattern = $this->route_collection->isThereAnyHow($name);
+		if($pattern)
+		{
+			$protocol = isset($_SERVER['HTTPS']) ? 'https://' : 'http://';
+			$server = $_SERVER['SERVER_NAME'] . '/';
+			$uri = [];
+			foreach(array_filter(explode('/', $_SERVER['REQUEST_URI'])) as $key => $value){
+				if($value == 'public') {
+					$uri[] = $value;
+					break;
+				}
+				$uri[] = $value;
+			}
+			$uri = implode('/', array_filter($uri)) . '/';
+			return $protocol . $server . $uri . $this->route_collection->convert($pattern, $params);
+		}
+		return false;
 	}
-  
-  //public static function getFileParamentos($pattern, $callback){
-    //echo "aqui";
-    //print_r($pattern);
-     //echo "<br>-------<br>";
-    //print_r($callback);
-      
-		//return self::getRouter()->get($pattern, $callback);
-	//}
-
-	public static function put($pattern, $callback){
-		return self::getRouter()->put($pattern, $callback);
-	}
-
-	public static function delete($pattern, $callback){
-		return self::getRouter()->delete($pattern, $callback);
-	}
-
-	public static function resolve($pattern){
-		return self::getRouter()->resolve($pattern);
-	}
-
-	public static function translate($pattern, $params){
-		return self::getRouter()->translate($pattern, $params);
-	}
-
 }
 
 ?>
